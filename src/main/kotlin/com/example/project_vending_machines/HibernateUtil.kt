b50228430a840ac1eb2908a1d608b82d70d82163
Pavel Kuzevanov <kuzya_pasha_run@mail.ru>
2026-02-24 11:52:29 +0500

    connection database (1) with errors

--- src/main/kotlin/com/example/project_vending_machines/HibernateUtil.kt
+++ src/main/kotlin/com/example/project_vending_machines/HibernateUtil.kt
@@ -1,6 +1,7 @@
 package com.example.project_vending_machines
 
-import java.lang.module.Configuration
+import org.hibernate.SessionFactory
+import org.hibernate.cfg.Configuration
 
 object HibernateUtil {
     val sessionFactory: SessionFactory = try {
